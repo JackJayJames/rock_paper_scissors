d779f3099bf3d128eae7d723d5b1fb5f466adb57
JackJayJames <115401650+JackJayJames@users.noreply.github.com>
2022-10-16 19:21:38 +0200

    progress
    can play rock paper scissors, and it can be started by button, but there is a bug which makes it go for more rounds than it needs to

--- javascript.js
+++ javascript.js
@@ -49,9 +49,93 @@ function getComputerChoice()
 
 function playround(player, computer)
 {
-    console.log(player + " - " + computer);
+    if(player == "rock")
+    {
+        if(computer == "paper")
+        {
+            return "computer";
+        }
+        else if(computer == "scissors")
+        {
+            return "player";
+        }
+        else
+        {
+            return "draw";
+        }
+    }
+    else if(player == "paper")
+    {
+        if(computer == "scissors")
+        {
+            return "computer";
+        }
+        else if(computer == "rock")
+        {
+            return "player";
+        }
+        else
+        {
+            return "draw";
+        }
+    }
+    else if(player == "scissors")
+    {
+        if(computer == "rock")
+        {
+            return "computer";
+        }
+        else if(computer == "paper")
+        {
+            return "player";
+        }
+        else
+        {
+            return "draw";
+        }
+    }
 }
 
-const player = getPlayerChoice();
+function game()
+{
+    let player;
+    let computer;
+    let result;
+    let player_score = 0;
+    let computer_score = 0;
+    for(let i = 0; i < 5; i++)
+    {
+        player = getPlayerChoice();
+        computer = getComputerChoice();
+        result = playround(player, computer);
+        console.log(player + "    " + computer);
+        console.log(result);
+        if(result == "player")
+        {
+            player_score++;
+            if(player == 5)
+            {
+                console.log("Player won");
+                break;
+            }
+        }
+        else if(result == "computer")
+        {
+            computer_score++;
+            if(computer == 5)
+            {
+                console.log("Computer won");
+                break;
+            }
+        }
+        else if(result == "draw")
+        {
+            i--;
+        }
+    }
+}
+
+/*const player = getPlayerChoice();
 const computer = getComputerChoice();
-console.log(playround(player, computer));
+console.log(player + "    " + computer);
+console.log(playround(player, computer));*/
